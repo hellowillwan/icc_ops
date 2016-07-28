458d8662f4abe4f932c715ac0018483fceda77a8
hellowillwan <wlong2000joint@gmail.com>
2016-07-28 12:16:49 +0800

    changes for weshop ui files sync add zhibo

--- scripts/WeshopCI.sh
+++ scripts/WeshopCI.sh
@@ -6,8 +6,9 @@
 # 变量
 #
 weshop_enabled_hosts='/var/lib/weshop_enabled_hosts'	# 开启微商功能的项目编号列表,一行一个
-weshop_m2_enabled_hosts='/var/lib/weshop_m2_enabled_hosts'	# 开启微商功能的项目编号列表,一行一个
 weshop_filelist='/var/lib/weshop_filelist'		# 需要更新的微商项目文件/目录,一行一个
+weshop_ui_enabled_hosts='/var/lib/weshop_ui_enabled_hosts'	# 开启微商 ui 的项目编号列表,一行一个
+weshop_ui_filelist='/var/lib/weshop_ui_filelist'		# 需要更新的微商 ui 文件/目录,一行一个
 webroot='/home/webs/dev'				# 内网dev环境(dev.umaman.xyz域名)的 WebRoot目录
 weshop_dir="${webroot}/weshop"				# 内网dev环境(dev.umaman.xyz域名)的 weshop 项目目录
 svncmd='/usr/bin/svn '
@@ -64,40 +65,46 @@ update_to_all_projects() {
 }
 
 # 同步线上生产环境 weshop 前端代码到 dev环境开启微商的具体项目
-weshop_prod_to_all_projects() {
+pull_weshop_prod_for_child_projects() {
 	# 默认更新到所有开启微商的项目,可以接受参数只更新到指定项目
 	if [ -z "$1" ];then
-		local projects=$(cat ${weshop_enabled_hosts})
+		local projects=$(cat ${weshop_ui_enabled_hosts})
 	else
 		local projects="$1"
 	fi
 
 	for project_code in ${projects} ;do
-		if test -d /home/webs/dev/${project_code}/public/html/m2 ;then 
-			# 如果该项目 存在 m2 目录,则排除 diff 目录进行同步
-			local src_dir='web/weshop/public/html/m2/'
-			#local src_dir='/home/webs/dev/weshop/public/html/m2/'
-			local dst_dir="/home/webs/dev/${project_code}/public/html/m2/"
-			is_exclude_diff=" --exclude='diff' "
-		else
-			# 如果该项目 不存在 m2 目录,则直接进行同步
-			local src_dir='web/weshop/public/html/m2'
-			#local src_dir='/home/webs/dev/weshop/public/html/m2'
-			local dst_dir="/home/webs/dev/${project_code}/public/html/"
-			is_exclude_diff=' '
-		fi
-		/bin/env USER='cutu5er' RSYNC_PASSWORD='1ccOper5' \
-		/usr/bin/rsync \
-		-vzrpt \
-		--blocking-io \
-		--exclude='svn' ${is_exclude_diff} \
-		211.152.60.33::${src_dir} ${dst_dir}	# 从线上正式环境拉取
-		#${src_dir} ${dst_dir}
+		for dir in $(cat $weshop_ui_filelist);do
+			echo "pull_weshop_prod_for_child_projects ${project_code} $dir"
+			if test -d ${webroot}/${project_code}${dir} ;then 
+				# 如果该项目 存在此目录,则排除 diff 目录进行同步
+				local src_dir="web/weshop${dir}/"
+				#local src_dir="${webroot}/weshop/${dir}/"
+				local dst_dir="${webroot}/${project_code}${dir}/"
+				is_exclude_diff=" --exclude='diff' "
+			else
+				# 如果该项目 不存在此目录,则直接进行同步
+				local src_dir="web/weshop${dir}"
+				#local src_dir="${webroot}/weshop/${dir}"
+				local dst_dir="${webroot}/${project_code}${dir}"
+				local dst_dir="${dst_dir%/*}/"
+				# 确保父目录存在
+				test -d ${dst_dir} || mkdir -p ${dst_dir}
+				is_exclude_diff=' '
+			fi
+			/bin/env USER='cutu5er' RSYNC_PASSWORD='1ccOper5' \
+			/usr/bin/rsync \
+			-vzrpt \
+			--blocking-io \
+			--exclude='svn' ${is_exclude_diff} \
+			211.152.60.33::${src_dir} ${dst_dir}	# 从线上正式环境拉取
+			#${src_dir} ${dst_dir}
+		done
 	done
 }
 
 # 为各个项目编译打包(webpack m2),并提交到项目SVN仓库
-pack_m2_and_commit() {
+pack_ui_and_commit() {
 	# 只更新到指定项目
 	if [ -z "$1" ];then
 		echo no project_code
@@ -107,27 +114,30 @@ pack_m2_and_commit() {
 	fi
 
 	for project in ${projects};do
-		workingdir="${webroot}/${project}/public/html/m2/"
-		if [ ! -d ${workingdir} ];then
-			echo "dir: ${workingdir} not exits."
-			continue
-		fi
-
-		# 删除项目 m2 目录下的 node_modules,并链接到全局目录
-		rm ${workingdir}/node_modules -rf
-		ln -s /var/lib/node_modules ${workingdir}
-		# 更新
-		# ${svncmd} ${svnoptions} up ${workingdir}
-		# 打包
-		( cd ${workingdir} ; gulp pro )
-		
-		# 删除项目 m2 目录下的 node_modules 准备提交 m2 到具体项目 svn 库
-		rm ${workingdir}/node_modules -rf
-		${svncmd} ${svnoptions} --force add ${workingdir}/ 2>&1
-		# ${svncmd} ${svnoptions} --force add ${workingdir}/dist/images
-		# ${svncmd} ${svnoptions} --force add ${workingdir}/dist/js/*.js
-		# ${svncmd} ${svnoptions} commit -m"update by weshop ci_tool ${message}" ${workingdir}/dist
-		${svncmd} ${svnoptions} commit -m"update by weshop ci_tool ${message}" ${workingdir}/ 2>&1
+		for dir in $(cat $weshop_ui_filelist);do
+			workingdir="${webroot}/${project}${dir}/"
+			echo "pack_ui_and_commit ${workingdir}"
+			if [ ! -d ${workingdir} ];then
+				echo "dir: ${workingdir} not exits."
+				continue
+			fi
+	
+			# 删除项目 ui 目录下的 node_modules,并链接到全局目录
+			rm ${workingdir}/node_modules -rf
+			ln -s /var/lib/node_modules ${workingdir}
+			# 更新
+			# ${svncmd} ${svnoptions} up ${workingdir}
+			# 打包
+			( cd ${workingdir} ; gulp pro )
+			
+			# 删除项目 ui 目录下的 node_modules 准备提交 m2 到具体项目 svn 库
+			rm ${workingdir}/node_modules -rf
+			${svncmd} ${svnoptions} --force add ${workingdir}/ 2>&1
+			# ${svncmd} ${svnoptions} --force add ${workingdir}/dist/images
+			# ${svncmd} ${svnoptions} --force add ${workingdir}/dist/js/*.js
+			# ${svncmd} ${svnoptions} commit -m"update by weshop ci_tool ${message}" ${workingdir}/dist
+			${svncmd} ${svnoptions} commit -m"update by weshop ci_tool ${message}" ${workingdir}/ 2>&1
+		done
 	done
 }
 
@@ -184,17 +194,20 @@ dev2demo() {
 		local projects="$1"
 	fi
 
+	# 指定目录
 	for project in ${projects} ;do
-		# 发布到demo
-		/bin/env USER='cutu5er' RSYNC_PASSWORD='1ccOper5' \
-		/usr/bin/rsync \
-		-vzrpt \
-		--blocking-io \
-		--exclude='svn' \
-		/home/webs/dev/${project}/public/html/m2 211.152.60.33::web/${project}demo/public/html/
-		# 分发到所有节点
-		localkey=$(date '+%Y-%m-%d'|tr -d '\n'|md5sum|cut -d ' ' -f 1)
-		echo $localkey sync_a_project_code ${project}demo | /usr/bin/gearman -h 211.152.60.33 -f CommonWorker_10.0.0.200
+		for dir in $(cat $weshop_ui_filelist);do
+			# 发布到demo
+			/bin/env USER='cutu5er' RSYNC_PASSWORD='1ccOper5' \
+			/usr/bin/rsync \
+			-vzrpt \
+			--blocking-io \
+			--exclude='svn' \
+			${webroot}/${project}${dir} 211.152.60.33::web/${project}demo${dir%/*}/
+			# 分发到所有节点
+			localkey=$(date '+%Y-%m-%d'|tr -d '\n'|md5sum|cut -d ' ' -f 1)
+			echo $localkey sync_a_project_code ${project}demo | /usr/bin/gearman -h 211.152.60.33 -f CommonWorker_10.0.0.200
+		done
 	done
 }
 
@@ -208,21 +221,21 @@ demo2prod() {
 }
 
 weshop_sync_prod() {
-	# 默认更新到所有开启微商m2的项目,可以接受参数只更新到指定项目
+	# 默认更新到所有开启微商 ui 的项目,可以接受参数只更新到指定项目
 	if [ -z "$1" ];then
-		local PROJECTS=$(cat ${weshop_m2_enabled_hosts})
+		local PROJECTS=$(cat ${weshop_ui_enabled_hosts})
 	else
 		local PROJECTS="$1"
 	fi
 
 	for project in ${PROJECTS} ;do
 		echo "$(date) 项目: $project"
-		echo -en "从线上 weshop 正式环境拉取 m2 目录\n\t"
-			weshop_prod_to_all_projects $project 2>&1 #|tail -n 1
-		echo -en "打包 m2 并提交到项目 $project SVN\n\t"
-			pack_m2_and_commit $project 2>&1 #|tail -n 1
+		echo -en "从线上 weshop 正式环境拉取 ui 相关目录\n\t"
+			pull_weshop_prod_for_child_projects $project 2>&1 #|tail -n 1
+		echo -en "打包 ui 目录 并提交到项目 $project SVN\n\t"
+			pack_ui_and_commit $project 2>&1 #|tail -n 1
 		echo -en "发布项目 $project 到 demo 环境\n\t"
-			dev2demo $project 2>&1 #|tail -n 1
+			dev2demo $project 2>&1 |tail -n 1
 		echo +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 	done
 }
@@ -235,4 +248,4 @@ weshop_sync_prod() {
 # /bin/env USER='cutu5er' RSYNC_PASSWORD='1ccOper5' /usr/bin/rsync -vzrptn --blocking-io --exclude='svn' --exclude='diff' 211.152.60.33::web/weshop/public/html/m2/ /home/webs/dev/140821fg0374/public/html/m2/
 
 # 手工执行
-# . /usr/local/sbin/WeshopCI.sh ; pack_m2_and_commit 140821fg0374
+# . /usr/local/sbin/WeshopCI.sh ; pack_ui_and_commit 140821fg0374
