039fd9b32871672f03e5c30a6dec7d2220f3de64
hellowillwan <wlong2000joint@gmail.com>
2017-02-08 18:46:30 +0800

    bash functions used in deepin

---
+++ scripts/deepin_bash_functions.sh
@@ -0,0 +1,76 @@
+#
+# bash functions
+#
+
+# start or stop smb service
+#
+sc() {
+	case "$1" in
+		smb)
+			local services='nmbd smbd'
+			;;
+		dlna)
+			local services='minidlna'
+			;;
+		*)
+			echo -e "Usage: ${FUNCNAME[0]} service_name [start|stop|restart]\n"
+			return
+			;;
+	esac
+
+	case "$2" in
+		start)
+			local action1=enable
+			local action2=start
+			;;
+		stop)
+			local action1=disable
+			local action2=stop
+			;;
+		restart)
+			local action1=enable
+			local action2=restart
+			;;
+		*)
+			echo -e "Usage: ${FUNCNAME[0]} service_name [start|stop|restart]\n"
+			;;
+	esac
+
+	for s in $services ;do
+		test -n "$action1" && (sudo systemctl $action1 $s)
+		test -n "$action2" && (sudo systemctl $action2 $s; if [ "$action2" = stop ];then sudo killall $s ;fi;sleep 1)
+		sudo systemctl status $s;
+		sudo ps -ef | grep -e $s | grep -v -e grep
+		echo
+	done
+}
+
+nt() {
+	case "$1" in
+		home)
+			sudo sed -i -e 's/^\(address1.*192.168.5.232.*\)/#\1/' \
+				-e 's/method=manual/method=auto/' \
+				/etc/NetworkManager/system-connections/有线连接
+			sudo systemctl restart NetworkManager
+			sleep 1
+			ip a l
+			;;
+		office)
+			sudo sed -i -e 's/method=auto/method=manual/' \
+				/etc/NetworkManager/system-connections/有线连接
+			#sudo sed -i -e 's/^#\(address1.*192.168.5.232.*\)/\1/' \
+			#	/etc/NetworkManager/system-connections/有线连接 || \
+			sudo sed -i -e '/ipv4/a\address1=192.168.5.232\/21,192.168.0.253' \
+				/etc/NetworkManager/system-connections/有线连接
+			sudo systemctl restart NetworkManager
+			sleep 1
+			ip a l
+			;;
+		*)
+			echo -e "Usage: ${FUNCNAME[0]} [home|office]\n"
+			sudo cat /etc/NetworkManager/system-connections/有线连接
+			echo
+			ip a l
+			;;
+	esac
+}
